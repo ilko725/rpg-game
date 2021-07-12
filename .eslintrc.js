a472f7c4fe090c86e9f34fef59eac20743df423d
ilko725 <ilko725@gmail.com>
2021-07-12 10:22:49 +0300

    homework-2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,20 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['airbnb-base'],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  rules: {},
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['airbnb-base'],
+    parserOptions: {
+        ecmaVersion: 12,
+        // sourceType: 'module',
+    },
+    rules: {
+        'quotes': 'off',
+        'no-unused-vars': 'off',
+        'no-restricted-syntax': 'off',
+        'guard-for-in': 'off',
+        'prefer-const': 'off',
+        'object-shorthand': 'off',
+        'object-curly-newline': 'off',
+    },
 };

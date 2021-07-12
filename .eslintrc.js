1fa2b27ba2b802bb091971e5f7fe6c81c8c956af
ilko725 <ilko725@gmail.com>
2021-07-12 15:44:03 +0300

    homework-2.4

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
         //'quotes': 'off', //delete later
         // 'no-unused-vars': 'off', //delete later
         'no-restricted-syntax': 'off', //delete later
-        'guard-for-in': 'off', //delete later
+        // 'guard-for-in': 'off', //delete later
         // 'prefer-const': 'off', //delete later
         // 'object-shorthand': 'off', //delete later
         // 'object-curly-newline': 'off', //delete later
--- src/client/ClientGame.js
+++ src/client/ClientGame.js
@@ -4,37 +4,37 @@ import sprites from '../configs/sprites';
 import levelCfg from '../configs/world.json';
 
 class ClientGame {
-    constructor(cfg) {
-        Object.assign(this, { cfg });
+  constructor(cfg) {
+    Object.assign(this, { cfg });
 
-        this.engine = this.createEngine();
-        this.map = this.createWorld();
-        this.initEngine();
-    }
+    this.engine = this.createEngine();
+    this.map = this.createWorld();
+    this.initEngine();
+  }
 
-    createEngine() {
-        return new ClientEngine(document.getElementById(this.cfg.tagId));
-    }
+  createEngine() {
+    return new ClientEngine(document.getElementById(this.cfg.tagId));
+  }
 
-    createWorld() {
-        return new ClientWorld(this, this.engine, levelCfg);
-    }
+  createWorld() {
+    return new ClientWorld(this, this.engine, levelCfg);
+  }
 
-    initEngine() {
-        this.engine.loadSprites(sprites).then(() => {
-            this.engine.on('render', ( /*_, time*/ ) => {
-                this.map.init();
-            });
-            this.engine.start();
-        });
-    }
+  initEngine() {
+    this.engine.loadSprites(sprites).then(() => {
+      this.engine.on('render', (/* _, time */) => {
+        this.map.init();
+      });
+      this.engine.start();
+    });
+  }
 
-    static init(cfg) {
-        if (!ClientGame.game) {
-            ClientGame.game = new ClientGame(cfg);
-            console.log('Game INIT');
-        }
+  static init(cfg) {
+    if (!ClientGame.game) {
+      ClientGame.game = new ClientGame(cfg);
+      console.log('Game INIT');
     }
+  }
 }
 
 export default ClientGame;

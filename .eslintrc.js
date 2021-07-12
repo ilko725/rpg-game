3c08d891e3f97032a2fe8f8334f8a2986068c075
ilko725 <ilko725@gmail.com>
2021-07-12 14:38:33 +0300

    homework-2.4

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,12 +9,12 @@ module.exports = {
         sourceType: 'module',
     },
     rules: {
-        'quotes': 'off', //delete later
+        //'quotes': 'off', //delete later
         'no-unused-vars': 'off', //delete later
         'no-restricted-syntax': 'off', //delete later
         'guard-for-in': 'off', //delete later
-        'prefer-const': 'off', //delete later
-        'object-shorthand': 'off', //delete later
+        // 'prefer-const': 'off', //delete later
+        // 'object-shorthand': 'off', //delete later
         // 'object-curly-newline': 'off', //delete later
         'max-len': ['error', { code: 120, comments: 140 }],
         'object-curly-newline': ["error", { "multiline": true, "minProperties": 10 }]

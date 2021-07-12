8ab5ee7f49d868e6657c3f2cfed43e5c691c8799
ilko725 <ilko725@gmail.com>
2021-07-12 15:44:31 +0300

    homework-2.4

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
         // 'prefer-const': 'off', //delete later
         // 'object-shorthand': 'off', //delete later
         // 'object-curly-newline': 'off', //delete later
-        'max-len': ['error', { code: 120, comments: 140 }],
+        // 'max-len': ['error', { code: 120, comments: 140 }],
         'object-curly-newline': ["error", { "multiline": true, "minProperties": 10 }]
     },
 };

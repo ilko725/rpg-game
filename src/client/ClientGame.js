9dff2b328732521f8114fc82f7577e5de6997f0a
ilko725 <ilko725@gmail.com>
2021-07-15 16:20:12 +0300

    homework-2.7

--- src/client/ClientGame.js
+++ src/client/ClientGame.js
@@ -5,71 +5,71 @@ import levelCfg from '../configs/world.json';
 import gameObjects from '../configs/gameObjects.json';
 
 class ClientGame {
-    constructor(cfg) {
-        // Object.assign(this, { cfg });
+  constructor(cfg) {
+    // Object.assign(this, { cfg });
 
-        this.cfg = cfg;
-        this.gameObjects = gameObjects;
-        this.player = null;
-        this.engine = this.createEngine();
-        this.map = this.createWorld();
-        this.initEngine();
-    }
+    this.cfg = cfg;
+    this.gameObjects = gameObjects;
+    this.player = null;
+    this.engine = this.createEngine();
+    this.map = this.createWorld();
+    this.initEngine();
+  }
 
-    setPlayer(player) {
-        this.player = player;
-    }
+  setPlayer(player) {
+    this.player = player;
+  }
 
-    createEngine() {
-        return new ClientEngine(document.getElementById(this.cfg.tagId));
-    }
+  createEngine() {
+    return new ClientEngine(document.getElementById(this.cfg.tagId));
+  }
 
-    createWorld() {
-        return new ClientWorld(this, this.engine, levelCfg);
-    }
+  createWorld() {
+    return new ClientWorld(this, this.engine, levelCfg);
+  }
 
-    initEngine() {
-        this.engine.loadSprites(sprites).then(() => {
-            this.map.init();
-            this.engine.on('render', (_, time) => {
-                this.map.render(time);
-            });
-            this.engine.start();
-            this.initKeys();
-        });
-    }
+  initEngine() {
+    this.engine.loadSprites(sprites).then(() => {
+      this.map.init();
+      this.engine.on('render', (_, time) => {
+        this.map.render(time);
+      });
+      this.engine.start();
+      this.initKeys();
+    });
+  }
 
-    initKeys() {
-        this.engine.input.onKey({
-            ArrowLeft: (keydown) => {
-                if (keydown) {
-                    this.player.moveByCellCoord(-1, 0, (cell) => cell.findObjectsByType('grass').length);
-                }
-            },
-            ArrowRight: (keydown) => {
-                if (keydown) {
-                    this.player.moveByCellCoord(1, 0, (cell) => cell.findObjectsByType('grass').length);
-                }
-            },
-            ArrowUp: (keydown) => {
-                if (keydown) {
-                    this.player.moveByCellCoord(0, -1, (cell) => cell.findObjectsByType('grass').length);
-                }
-            },
-            ArrowDown: (keydown) => {
-                if (keydown) {
-                    this.player.moveByCellCoord(0, 1, (cell) => cell.findObjectsByType('grass').length);
-                }
-            },
-        });
-    }
-
-    static init(cfg) {
-        if (!ClientGame.game) {
-            ClientGame.game = new ClientGame(cfg);
-            console.log('Game INIT');
+  initKeys() {
+    this.engine.input.onKey({
+      ArrowLeft: (keydown) => {
+        if (keydown) {
+          this.player.moveByCellCoord(-1, 0, (cell) => cell.findObjectsByType('grass').length);
         }
+      },
+      ArrowRight: (keydown) => {
+        if (keydown) {
+          this.player.moveByCellCoord(1, 0, (cell) => cell.findObjectsByType('grass').length);
+        }
+      },
+      ArrowUp: (keydown) => {
+        if (keydown) {
+          this.player.moveByCellCoord(0, -1, (cell) => cell.findObjectsByType('grass').length);
+        }
+      },
+      ArrowDown: (keydown) => {
+        if (keydown) {
+          this.player.moveByCellCoord(0, 1, (cell) => cell.findObjectsByType('grass').length);
+        }
+      },
+    });
+  }
+
+  static init(cfg) {
+    if (!ClientGame.game) {
+      ClientGame.game = new ClientGame(cfg);
+      console.log('Game INIT');
     }
+  }
 }
 
 export default ClientGame;
--- src/common/util.js
+++ src/common/util.js
@@ -1,7 +1,7 @@
 export default function clamp(x, fromX, toX) {
-    let temporaryX = 0;
-    if (x < fromX) temporaryX = fromX;
-    if (x > toX) temporaryX = toX;
+  let temporaryX = 0;
+  if (x < fromX) temporaryX = fromX;
+  if (x > toX) temporaryX = toX;
 
-    return temporaryX;
+  return temporaryX;
 }
